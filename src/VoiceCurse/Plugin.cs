f2969961e11b110cdf5e85e39f134d986c668b99
Ryocery <youri.haentjens@gmail.com>
2025-11-22 11:42:17 +0100

    initialize project structure with essential configuration files and plugin setup

---
+++ src/VoiceCurse/Plugin.cs
@@ -0,0 +1,14 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+
+namespace VoiceCurse;
+
+[BepInAutoPlugin]
+public partial class Plugin : BaseUnityPlugin {
+    internal static ManualLogSource Log { get; set; } = null!;
+
+    private void Awake() {
+        Log = Logger;
+        Log.LogInfo($"Plugin {Name} is loaded!");
+    }
+}
